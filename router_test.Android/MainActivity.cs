885644403a68321dbb08a33cdacd052fe81a58b4
Mrcubix <tintamare91@gmail.com>
2022-09-19 20:45:34 +0200

    Added Wild Android Project

---
+++ router_test.Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia.Android;
+using Avalonia;
+
+namespace router_test.Android
+{
+    [Activity(Label = "router_test.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+    public class MainActivity : AvaloniaActivity<App>
+    {
+        protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+        {
+            return base.CustomizeAppBuilder(builder);
+        }
+    }
+}
